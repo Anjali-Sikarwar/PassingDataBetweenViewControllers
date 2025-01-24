a767027afb8e79630b44f2d2439268cbb1cf8520
Anjali-Sikarwar <sikarwar.official029@gmail.com>
2025-01-24 21:34:23 +0530

    Initial Commit

---
+++ PassingDataBetweenViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassingDataBetweenViewControllers
+//
+//  Created by Anjali Sikarwar on 24/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
